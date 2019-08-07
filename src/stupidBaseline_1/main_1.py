fdc81b3c6bbef902d4375f57b0e45cc19dcfb1c8
ddatta <ddatta@vt.edu>
2019-08-06 23:59:43 -0400

    update

--- src/stupidBaseline_1/main_1.py
+++ src/stupidBaseline_1/main_1.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn
 from scipy.sparse import csr_matrix
 from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 import pickle
 from sklearn.metrics import auc
 from sklearn.decomposition import TruncatedSVD
@@ -34,11 +35,19 @@ clf = IsolationForest(
     max_samples=10000,
     contamination=0.0,
     n_estimators=1000,
-    behaviour ='new'
+    behaviour = 'old'
 )
 clf.fit(_train_x)
+
+
+clf_1 = LocalOutlierFactor(
+    n_neighbors=20,
+    novelty =True
+)
+clf_1.fit(_train_x)
+
 _test_x = tsvd.transform(test_x)
-result = clf.score_samples(_test_x)
+result = clf_1.score_samples(_test_x)
 
 res = list(result)
 _id_score_dict = {
@@ -53,9 +62,20 @@ sorted_id_score_dict = OrderedDict()
 for e in tmp:
     sorted_id_score_dict[e[0]] = e[1]
 
+bounds = []
+# training_pos_scores = clf.score_samples(
+#     _train_x
+# )
+# training_pos_scores = [_[0] for _ in training_pos_scores]
+from pprint import pprint
+
+pprint(result)
+bounds.append(-1)
+bounds.append(0)
 recall, precison = eval.precision_recall_curve(
     sorted_id_score_dict,
-    anomaly_id_list=anom_ids
+    anomaly_id_list=anom_ids,
+    bounds=bounds
 )
 
 _auc = auc(recall, precison)
